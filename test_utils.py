bf5ae0746446c78ca16cfff89b3580a9022975f5
Can Bakır <canbakir98@gmail.com>
2022-03-10 03:03:39 +0300

    Functions Fixed

--- test_utils.py
+++ test_utils.py
@@ -5,25 +5,28 @@ import contractions
 import re
 from nltk.tokenize import word_tokenize
 
-try:
-    with open("saved-models/SVM_E-I.sav", "rb") as file:
-        ei_classifier = pickle.load(file)
-    with  open("saved-models/Xgboost_N-S.sav", "rb") as file:
-        ns_classifier = pickle.load(file)
-    with open("saved-models/SVM_F-T.sav", "rb") as file:
-        ft_classifier = pickle.load(file)
-    with  open("saved-models/Xgboost_J-P.sav", "rb") as file:
-        jp_classifier = pickle.load(file)
-except FileNotFoundError:
-    print("Model not found!")
+def load_files():
+    try:
+        with open("saved-models/SVM_E-I.sav", "rb") as file:
+            ei_classifier = pickle.load(file)
+        with  open("saved-models/Xgboost_N-S.sav", "rb") as file:
+            ns_classifier = pickle.load(file)
+        with open("saved-models/SVM_F-T.sav", "rb") as file:
+            ft_classifier = pickle.load(file)
+        with  open("saved-models/Xgboost_J-P.sav", "rb") as file:
+            jp_classifier = pickle.load(file)
+    except FileNotFoundError:
+        print("Model not found!")
 
-try:
-    with open("vectorizer/vectorizer.pkl", "rb") as file:
-        vectorizer = pickle.load(file)
-except FileNotFoundError:
-    print("Tokenizer not found!")
+    try:
+        with open("vectorizer/vectorizer.pkl", "rb") as file:
+            vectorizer = pickle.load(file)
+    except FileNotFoundError:
+        print("Tokenizer not found!")
+
+    return ei_classifier, ns_classifier, ft_classifier, jp_classifier, vectorizer
     
-def preprocessing(test, vectorizer, ei_classifier, ns_classifier, ft_classifier, jp_classifier):
+def preprocessing(test):
     stopword_list = stopwords.words("english")
     lemmatizer = WordNetLemmatizer()
     
@@ -37,6 +40,11 @@ def preprocessing(test, vectorizer, ei_classifier, ns_classifier, ft_classifier,
     test = [word for word in test if not word in stopword_list]
     test = [lemmatizer.lemmatize(word) for word in test]
     test = " ".join(test)
+    return test
+
+def get_prediction(test):
+    ei_classifier, ns_classifier, ft_classifier, jp_classifier, vectorizer = load_files()
+    test = preprocessing(test)
     test = vectorizer.transform([test])
     
     prediction = ""
@@ -45,5 +53,5 @@ def preprocessing(test, vectorizer, ei_classifier, ns_classifier, ft_classifier,
     f_or_t = "F" if ft_classifier.predict(test)[0] == 1 else "T"
     j_or_p = "J" if jp_classifier.predict(test)[0] == 1 else "P"
     prediction = e_or_i + n_or_s + f_or_t + j_or_p
-    
+
     return prediction
