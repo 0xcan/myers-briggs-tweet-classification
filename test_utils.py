86cf6083757d1b71b2127c5212ba6d408254cbe8
Can Bakır <canbakir98@gmail.com>
2022-03-14 16:25:38 +0300

    Search with Username Added

--- test_utils.py
+++ test_utils.py
@@ -17,11 +17,11 @@ def get_tweets(username):
 
 def load_files():
     try:
-        with open("saved-models/SVM_E-I.sav", "rb") as file:
+        with open("saved-models/Xgboost_E-I.sav", "rb") as file:
             ei_classifier = pickle.load(file)
         with  open("saved-models/Xgboost_N-S.sav", "rb") as file:
             ns_classifier = pickle.load(file)
-        with open("saved-models/SVM_F-T.sav", "rb") as file:
+        with open("saved-models/Xgboost_F-T.sav", "rb") as file:
             ft_classifier = pickle.load(file)
         with  open("saved-models/Xgboost_J-P.sav", "rb") as file:
             jp_classifier = pickle.load(file)
@@ -53,8 +53,9 @@ def preprocessing(test):
     test = " ".join(test)
     return test
 
-def get_prediction(test):
+def get_prediction(username):
     ei_classifier, ns_classifier, ft_classifier, jp_classifier, vectorizer = load_files()
+    test = get_tweets(username)
     test = preprocessing(test)
     test = vectorizer.transform([test])
     
