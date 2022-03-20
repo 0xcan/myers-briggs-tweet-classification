877b4c7a8fd6d79480aee1de2772c80141272a70
Can Bakır <canbakir98@gmail.com>
2022-03-20 16:08:22 +0300

    Saved Models Changed

--- test_utils.py
+++ test_utils.py
@@ -16,11 +16,11 @@ def get_tweets(username):
 
 def load_files():
     try:
-        with open("saved-models/Xgboost_E-I.sav", "rb") as file:
+        with open("saved-models/RandomForest_E-I.sav", "rb") as file:
             ei_classifier = pickle.load(file)
-        with  open("saved-models/Xgboost_N-S.sav", "rb") as file:
+        with  open("saved-models/RandomForest_N-S.sav", "rb") as file:
             ns_classifier = pickle.load(file)
-        with open("saved-models/Xgboost_F-T.sav", "rb") as file:
+        with open("saved-models/SVM_F-T.sav", "rb") as file:
             ft_classifier = pickle.load(file)
         with  open("saved-models/Xgboost_J-P.sav", "rb") as file:
             jp_classifier = pickle.load(file)
